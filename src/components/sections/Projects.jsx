cd4665a1d4f858b8252165159a4b013a853ffc94
Hritik Raj <rajhritik36@gmail.com>
2025-03-22 13:32:24 +0530

    Describe the changes you made

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -33,7 +33,7 @@ export const Projects = () => {
 
               <div className="flex justify-between items-center">
                 <a
-                  href="#"
+                  href="https://github.com/ritzraj-23/weather-app"
                   className="text-blue-400 hover:text-blue-300 transition-colors my-4"
                 >
                   View Project →
